5ed48941d5567900023c664f7e1bc5d91520ff80
Will Potter <potter@askspoke.com>
2018-11-28 10:04:54 -0800

    tweak

--- astronaut/download.js
+++ astronaut/download.js
@@ -7,10 +7,18 @@ const Logger = require('../common/logger');
 
 let latestImageUrl;
 let oldFileName;
+let imageTime;
 
 async function fetchLatestImage() {
   Logger.info('Checking for the latest image');
-  const {latest} = await fetchState();
+  const {latest, latestImageTime} = await fetchState();
+  if (!latest) {
+    return;
+  }
+  if (latestImageTime) {
+    imageTime = new Date(latestImageTime);
+    Logger.info(`Fetching image from: ${imageTime}`);
+  }
   Logger.info('Latest image url: ' + latest);
   if (latest === latestImageUrl) {
     Logger.info('No new image, won’t redownload');
@@ -24,17 +32,21 @@ async function fetchLatestImage() {
 
   const fileName = latest.replace(/\//gi, '_').replace('output_', '');
   Logger.info('Downloading image...');
-  const result = await rp.get(options)
-  const buffer = Buffer.from(result, 'utf8');
-  Logger.info('Saving file...');
-  await saveFile(fileName, buffer);
-  Logger.info('Done!')
-  const path = `${__dirname}/${fileName}`;
-  setWallpaper(path);
-  if (oldFileName) {
-    await deleteFile(oldFileName);
+  try {
+    const result = await rp.get(options)
+    const buffer = Buffer.from(result, 'utf8');
+    Logger.info('Saving file...');
+    await saveFile(fileName, buffer);
+    Logger.info('Done!')
+    const path = `${__dirname}/${fileName}`;
+    setWallpaper(path);
+    if (oldFileName) {
+      await deleteFile(oldFileName);
+    }
+    oldFileName = fileName;
+  } catch (e) {
+    return;
   }
-  oldFileName = fileName;
 }
 
 module.exports = {fetchLatestImage};
